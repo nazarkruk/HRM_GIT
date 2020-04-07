a04dd3005ff2af3aa12f09355a5933aac4157853
nazarkruk <nazarkruk@icloud.com>
2020-04-07 14:56:55 -0400

    imigration tests

--- tests/immigration_test.py
+++ tests/immigration_test.py
@@ -7,7 +7,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.support.wait import WebDriverWait
 
-
+####
 class ContactDetailsTestCase(unittest.TestCase):
 
     def setUp(self) -> None:
