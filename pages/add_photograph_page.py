dfd9c40c2215254f0a756ff9903a3e4d134171d6
nazarkruk <nazarkruk@icloud.com>
2020-04-08 15:42:08 -0400

    imigration testsd

--- pages/add_photograph_page.py
+++ pages/add_photograph_page.py
@@ -1,8 +1,4 @@
 
-from selenium import webdriver
-from fixtures.params import DOMAIN, CHROME_EXECUTABLE_PATH
-from pages.login_page import LoginPage
-from pages.personal_details_page import PersonalDetailsPage
 
 
 class AddPhotographPage:
@@ -10,13 +6,12 @@ class AddPhotographPage:
     def __init__(self, driver):
         self.driver = driver
 
-    def employee_picture(self):
+    def emloyee_picture(self):
         self.driver.find_element_by_id('empPic').click()
 
-    def choose_file(self, file_path):
+    def chose_file(self, file_path):
         self.driver.find_element_by_name('photofile').send_keys(file_path)
 
-    def save_button(self):
-
+    def upload_button(self):
         self.driver.find_element_by_id('btnSave').click()
 
